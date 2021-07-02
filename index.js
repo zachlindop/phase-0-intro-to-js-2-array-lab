98c8ff91ffc6e5b058f97df5d625a2c1f041b1f6
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-02 15:18:52 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+function destructivelyPrependCat(){
+      cats.unshift('Bob');
+  }   
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+  function destructivelyRemoveFirstCat(){
+      cats.shift();
+  }
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+  function appendCat(name){
+    const newCat = [...cats, 'Broom'];
+    return newCat;
+  } 
+  function prependCat(name){
+    const nCat = ['Arnold', ...cats];
+    return nCat;
+  }
+  function removeLastCat(){
+      const nCats = cats.slice(0, cats.length -1);
+      return nCats;
+  }
+  function removeFirstCat(){
+      const nnCats = cats.slice(1);
+      return nnCats;
+  }
